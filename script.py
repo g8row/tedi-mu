2baf276758ed3c1d57b00925c735038e83469284
guroww <aliogu23@gmail.com>
2023-10-03 21:43:34 +0300

    updated

--- script.py
+++ script.py
@@ -2,14 +2,14 @@ import requests
 import os
 from pypdf import PdfMerger
 
-page = 1
 pageStart= 1
-pageEnd = 1090
+pageEnd = 488
+doid = 5657
 
-for page in range(pageStart,pageEnd):
-    stri = 'https://ilib.mu-pleven.bg/bg/getfile.php?iid=DO-L20004944&doid=5296&page='
+for page in range(pageStart,pageEnd+1):
+    stri = 'https://ilib.mu-pleven.bg/bg/getfile.php?iid=DO-L10000514&doid=' + doid + "&page="
     if(page<10):
-        stri+='000'+str(page)+'%27'
+        stri+='000'+str(page)
     elif page<100:
         stri+='00'+str(page)
     elif page<1000:
@@ -18,17 +18,17 @@ for page in range(pageStart,pageEnd):
         stri+=str(page)
     print(stri)
     x = requests.get(stri)
-    with open(str(page)+'.pdf', "wb") as f:
+    with open('temp/'+str(page)+'.pdf', "wb") as f:
         f.write(x.content)
     page+=1
 
 merger = PdfMerger()
 
-for page in range(pageStart,pageEnd):
-    merger.append(str(page)+'.pdf')
+for page in range(pageStart,pageEnd+1):
+    merger.append('temp/'+str(page)+'.pdf')
 
-merger.write("result.pdf")
+merger.write(doid+".pdf")
 merger.close()
 
-for page in range(pageStart,pageEnd):
-    os.remove(str(page)+'.pdf')    
+for page in range(pageStart,pageEnd+1):
+    os.remove("temp/"+str(page)+'.pdf')    
